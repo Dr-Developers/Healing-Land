0bd00ac1e1f027383952b38e83ade274c60069e5
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-11-13 12:44:22 +0530

    add garden tips java

--- app/src/main/java/com/scorpion/healingland/AddGardenTips.java
+++ app/src/main/java/com/scorpion/healingland/AddGardenTips.java
@@ -2,41 +2,50 @@ package com.scorpion.healingland;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.scorpion.healingland.Database.DBHandler;
 
 import android.os.Bundle;
 
-public class AddGardenTips extends AppCompatActivity {
+public class AddGardenTips extends Fragment {
 
+    Button addNewTip;
     TextView errorMsg;
     TextInputEditText plantCode, plantName, botanicalName, plantType, water, plantingTip, fertilizingTip, imageUrl;
     Button submit;
     boolean status;
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_garden_tips);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        errorMsg = findViewById(R.id.errorText);
-        plantCode = findViewById(R.id.plantCode);
-        plantName = findViewById(R.id.plantName);
-        botanicalName = findViewById(R.id.botanicalName);
-        plantType = findViewById(R.id.plantType);
-        water = findViewById(R.id.water);
-        plantingTip = findViewById(R.id.plantingTip);
-        fertilizingTip = findViewById(R.id.fertilizer);
-        imageUrl = findViewById(R.id.uploadAPhoto);
-        submit = findViewById(R.id.addTipsBtn);
+        View view = inflater.inflate(R.layout.activity_add_garden_tips, container, false);
+
+        errorMsg = view.findViewById(R.id.errorText);
+        plantCode = view.findViewById(R.id.plantCode);
+        plantName = view.findViewById(R.id.plantName);
+        botanicalName = view.findViewById(R.id.botanicalName);
+        plantType = view.findViewById(R.id.plantType);
+        water = view.findViewById(R.id.water);
+        plantingTip = view.findViewById(R.id.plantingTip);
+        fertilizingTip = view.findViewById(R.id.fertilizer);
+        imageUrl = view.findViewById(R.id.uploadAPhoto);
+        submit = view.findViewById(R.id.addTipsBtn);
 
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -46,25 +55,29 @@ public class AddGardenTips extends AppCompatActivity {
                 if (plantCode.getText().toString().isEmpty() || plantName.getText().toString().isEmpty() || botanicalName.getText().toString().isEmpty() || plantType.getText().toString().isEmpty() || water.getText().toString().isEmpty() || plantingTip.getText().toString().isEmpty() || fertilizingTip.getText().toString().isEmpty() || imageUrl.getText().toString().isEmpty()) {
                     errorMsg.setText("* All the fields are required");
                 } else {
-                        DBHandler dbHandler = new DBHandler(getApplicationContext());
-                        long newID = dbHandler.AddGardenTips(plantCode.getText().toString(), plantName.getText().toString(), botanicalName.getText().toString(), plantType.getText().toString(), water.getText().toString(), plantingTip.getText().toString(), fertilizingTip.getText().toString(), imageUrl.getText().toString());
+                    DBHandler dbHandler = new DBHandler(getActivity());
+                    long newID = dbHandler.AddGardenTips(plantCode.getText().toString(), plantName.getText().toString(), botanicalName.getText().toString(), plantType.getText().toString(), water.getText().toString(), plantingTip.getText().toString(), fertilizingTip.getText().toString(), imageUrl.getText().toString());
 
-                        System.out.println(newID);
+                    System.out.println(newID);
 
-                        if (newID >= 1) {
-                            Toast.makeText(AddGardenTips.this, "Garden Tip Added Successfully!", Toast.LENGTH_SHORT).show();
+                    if (newID >= 1) {
+                        Toast.makeText(getActivity(), "Garden Tip Added Successfully!", Toast.LENGTH_SHORT).show();
 
-
-                            // checking the login status
-
-                        } else {
-                            Toast.makeText(AddGardenTips.this, "Adding Garden Tip is Failed!", Toast.LENGTH_SHORT).show();
-                        }
+                        TipsList tipsList = new TipsList();
+                        FragmentManager fragmentManager = getFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.fragment_container, tipsList);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        // checking the login status
+                    } else {
+                        Toast.makeText(getActivity(), "Adding Garden Tip is Failed!", Toast.LENGTH_SHORT).show();
+                    }
 
                 }
             }
         });
-
+        return view;
     }
 
 //    public void navigateLogin(View view) {
