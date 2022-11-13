28c97c925ec6703422d1e2526e240e0741da2356
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2022-11-13 12:45:11 +0530

    gerbera plant view java

--- app/src/main/java/com/scorpion/healingland/GerberaPlantView.java
+++ app/src/main/java/com/scorpion/healingland/GerberaPlantView.java
@@ -1,14 +1,56 @@
+//package com.scorpion.healingland;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.os.Bundle;
+//
+//public class GerberaPlantView extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_gerbera_plant_view);
+//    }
+//}
+
 package com.scorpion.healingland;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class GerberaPlantView extends AppCompatActivity {
+public class GerberaPlantView extends Fragment {
+
+    Button Gerbera_Update;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gerbera_plant_view);
+    @Nullable
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_rose_view, container,false);
+
+        Gerbera_Update =view.findViewById(R.id.Rose_Update);
+
+        Gerbera_Update.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditGardenTips editGardenTips = new EditGardenTips();
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, editGardenTips);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
     }
 }
